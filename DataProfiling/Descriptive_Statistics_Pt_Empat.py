aa3f313975aff51ead5e687d1d7b2cb9c67ba42e
Ryan <ryanrizkyf@gmail.com>
2021-02-25 12:21:34 +0700

    edit commit

---
+++ DataProfiling/Descriptive_Statistics_Pt_Empat.py
@@ -0,0 +1,77 @@
+# Maximum dan Minimum
+# Fungsi max dan min digunakan untuk mengetahui element terbesar dan terkecil dari suatu kolom di dataframe.
+# nama_dataframe["nama_kolom"].max()
+# nama_dataframe["nama_kolom"].min()
+
+# Mean, Medium, Modus dan Standard Deviasi
+# Fungsi mean, medium, modus dan standard deviasi digunakan untuk mengetahui pemusatan data dan persebarannya.
+# nama_dataframe["nama_kolom"].mean()
+# nama_dataframe["nama_kolom"].median()
+# nama_dataframe["nama_kolom"].mode()
+# nama_dataframe["nama_kolom"].std()
+
+# Dataset : 'https://storage.googleapis.com/dqlab-dataset/retail_raw_reduced_data_quality.csv'.
+
+import pandas as pd
+import numpy as np
+import io
+import pandas_profiling
+retail_raw = pd.read_csv(
+    'retail_raw_reduced_data_quality.csv')
+
+# Cetak tipe data di setiap kolom retail_raw
+print(retail_raw.dtypes)
+
+# Kolom city
+length_city = len(retail_raw['city'])
+print('Length kolom city:', length_city)
+
+# Tugas Praktek: Kolom product_id
+length_product_id = len(retail_raw['product_id'])
+print('Length kolom product_id:', length_product_id)
+
+# Count kolom city
+count_city = retail_raw['city'].count()
+print('Count kolom count_city:', count_city)
+
+# Tugas praktek: count kolom product_id
+count_product_id = retail_raw['product_id'].count()
+print('Count kolom product_id:', count_product_id)
+
+# Missing value pada kolom city
+number_of_missing_values_city = length_city - count_city
+float_of_missing_values_city = float(number_of_missing_values_city/length_city)
+pct_of_missing_values_city = '{0:.1f}%'.format(
+    float_of_missing_values_city * 100)
+print('Persentase missing value kolom city:', pct_of_missing_values_city)
+
+# Tugas praktek: Missing value pada kolom product_id
+number_of_missing_values_product_id = length_product_id - count_product_id
+float_of_missing_values_product_id = float(
+    number_of_missing_values_product_id/length_product_id)
+pct_of_missing_values_product_id = '{0:.1f}%'.format(
+    float_of_missing_values_product_id * 100)
+print('Persentase missing value kolom product_id:',
+      pct_of_missing_values_product_id)
+
+# Deskriptif statistics kolom quantity
+print('Kolom quantity')
+print('Minimum value: ', retail_raw['quantity'].min())
+print('Maximum value: ', retail_raw['quantity'].max())
+print('Mean value: ', retail_raw['quantity'].mean())
+print('Mode value: ', retail_raw['quantity'].mode())
+print('Median value: ', retail_raw['quantity'].median())
+print('Standard Deviation value: ', retail_raw['quantity'].std())
+
+# Tugas praktek:
+# Buatlah statistics deskriptif untuk data item_price dari dataframe retail_raw!
+# Berupa max, min, mean, median, dan std!
+
+# Tugas praktek: Deskriptif statistics kolom item_price
+print('')
+print('Kolom item_price')
+print('Minimum value: ', retail_raw['item_price'].min())
+print('Maximum value: ', retail_raw['item_price'].max())
+print('Mean value: ', retail_raw['item_price'].mean())
+print('Median value: ', retail_raw['item_price'].median())
+print('Standard Deviation value: ', retail_raw['item_price'].std())
