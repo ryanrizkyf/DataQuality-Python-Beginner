7c15da38485369413a9b92621341e08e0bcfa4c5
Ryan <ryanrizkyf@gmail.com>
2021-02-25 18:17:50 +0700

    edit commit

---
+++ MiniProject/MiniProject.py
@@ -0,0 +1,10 @@
+# 1. Case Study: Data Profiling
+# Tolong proses dataset terlampir yang  disimpan dalam bentuk csv bernama
+# 'https://storage.googleapis.com/dqlab-dataset/uncleaned_raw.csv'.
+
+# Anda bisa memprosesnya dengan cara berikut:
+# 1. Import dataset csv ke variable bernama uncleaned_raw.
+# 2. Inspeksi dataframe uncleaned_raw.
+# 3. Check kolom yang mengandung missing value, jika ada,
+# kolom apakah itu dan berapa persen missing value pada kolom tersebut?
+# 4. Mengisi missing value tersebut dengan mean dari kolom tersebut!
